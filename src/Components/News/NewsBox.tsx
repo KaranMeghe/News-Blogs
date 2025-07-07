096a1608ec668bd9b530f0babec6248696ac8598
Karan Meghe <karanmeghe87@gmail.com>
2025-07-07 15:45:57 +0530

    feat(news ui): initial ui for news headline and news box

--- src/Components/News/NewsBox.tsx
+++ src/Components/News/NewsBox.tsx
@@ -1,9 +1,62 @@
 /** @format */
 
+import techImg from '../../assets/images/tech.jpg';
+import sportImg from '../../assets/images/sports.jpg';
+import sciImg from '../../assets/images/science.jpg';
+import healthImg from '../../assets/images/health.jpg';
+import worldImg from '../../assets/images/world.jpg';
+import { FaBookmark } from 'react-icons/fa';
+
 const NewsBox = () => {
   return (
-    <article className='bg-[#111214] h-full rounded-md'>
-      <div>News Grid</div>
+    <article className='bg-[#111214] h-full rounded-md flex justify-around gap-8'>
+      <div>
+        <img src={techImg} />
+        <h3>
+          Lorem ipsum dolor sit amet.
+          <FaBookmark />
+        </h3>
+      </div>
+
+      <div>
+        <img src={techImg} />
+        <h3>
+          Lorem ipsum dolor sit amet.
+          <FaBookmark />
+        </h3>
+      </div>
+
+      <div>
+        <img src={techImg} />
+        <h3>
+          Lorem ipsum dolor sit amet.
+          <FaBookmark />
+        </h3>
+      </div>
+
+      <div>
+        <img src={techImg} />
+        <h3>
+          Lorem ipsum dolor sit amet.
+          <FaBookmark />
+        </h3>
+      </div>
+
+      <div>
+        <img src={techImg} />
+        <h3>
+          Lorem ipsum dolor sit amet.
+          <FaBookmark />
+        </h3>
+      </div>
+
+      <div>
+        <img src={techImg} />
+        <h3>
+          Lorem ipsum dolor sit amet.
+          <FaBookmark />
+        </h3>
+      </div>
     </article>
   );
 };
--- src/Components/News/NewsHeadline.tsx
+++ src/Components/News/NewsHeadline.tsx
@@ -1,9 +1,19 @@
 /** @format */
+import { FaBookmark } from 'react-icons/fa6';
+import techImg from '../../assets/images/tech.jpg';
 
 const NewsHeadline = () => {
   return (
-    <article className=' bg-[#111214] w-full h-[55%] mb-2 rounded-md'>
-      <div>Headline of the year</div>
+    <article className='w-full h-[55%] mb-2 rounded-md relative overflow-hidden'>
+      <img src={techImg} alt='Headline' className='h-full w-full object-cover' />
+
+      <div className='absolute bottom-0 left-0 w-full bg-black/60 backdrop-blur-sm p-4 flex items-center justify-between gap-4'>
+        <h2 className='text-white text-lg md:text-2xl font-medium tracking-wide'>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Asperiores suscipit magnam soluta voluptate dolorem
+          quam, vero libero repudiandae.
+        </h2>
+        <FaBookmark className='text-gray-300 text-5xl hover:text-white transition-colors cursor-pointer' />
+      </div>
     </article>
   );
 };
